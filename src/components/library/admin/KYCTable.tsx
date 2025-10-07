cd6115539d3357c8b8dd1b7307033f9462d7c967
Purushothaman G <officialpurushothaman@gmail.com>
2025-10-07 23:02:14 +0530

    refactor: scrollable table for kyc

--- src/components/library/admin/KYCTable.tsx
+++ src/components/library/admin/KYCTable.tsx
@@ -1,41 +1,28 @@
-import {
-  ButtonGroup,
-  IconButton,
-  Pagination,
-  Stack,
-  Table,
-} from "@chakra-ui/react";
+import { Table } from "@chakra-ui/react";
 import { format } from "date-fns";
-import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
-import ViewDocsBtn from "./ViewDocsBtn";
 import { useAdminGetKYCs } from "../../hooks/admin/useKyc";
+import ViewDocsBtn from "./ViewDocsBtn";
 
 const KYCTable = () => {
   const {
     data,
-    hasNextPage,
-    hasPreviousPage,
-    fetchNextPage,
-    fetchPreviousPage,
+    // hasNextPage,
+    // hasPreviousPage,
+    // fetchNextPage,
+    // fetchPreviousPage,
   } = useAdminGetKYCs();
 
-  console.log(
-    data?.pages[(data.pageParams[0] as number) - 1].data.data.totalPages
-  );
-
-  const handlePrev = () => {
-    if (hasPreviousPage) fetchPreviousPage();
-  };
-
-  const handleNext = () => {
-    if (hasNextPage) fetchNextPage();
-  };
-
   return (
-    <Stack width="full" gap="5">
-      <Table.Root size="sm" variant="outline" striped borderRadius="xl">
+    <Table.ScrollArea
+      w="full"
+      borderRadius="xl"
+      borderWidth="1px"
+      rounded="xl"
+      height="100%"
+    >
+      <Table.Root size="md" stickyHeader>
         <Table.Header>
-          <Table.Row>
+          <Table.Row bg="bg.subtle">
             <Table.ColumnHeader>Name</Table.ColumnHeader>
             <Table.ColumnHeader textAlign="center">
               Guest Count
@@ -63,40 +50,7 @@ const KYCTable = () => {
           )}
         </Table.Body>
       </Table.Root>
-
-      <Pagination.Root
-        count={
-          (data?.pages[(data.pageParams[0] as number) - 1].data.data
-            .totalPages || 1) * 5
-        }
-        pageSize={
-          data?.pages[(data.pageParams[0] as number) - 1].data.data.limit
-        }
-        page={(data?.pageParams[0] as number) || 1}
-      >
-        <ButtonGroup variant="ghost" size="sm" wrap="wrap">
-          <Pagination.PrevTrigger asChild onClick={handlePrev}>
-            <IconButton>
-              <LuChevronLeft />
-            </IconButton>
-          </Pagination.PrevTrigger>
-
-          <Pagination.Items
-            render={(page) => (
-              <IconButton variant={{ base: "ghost", _selected: "outline" }}>
-                {page.value}
-              </IconButton>
-            )}
-          />
-
-          <Pagination.NextTrigger asChild onClick={handleNext}>
-            <IconButton>
-              <LuChevronRight />
-            </IconButton>
-          </Pagination.NextTrigger>
-        </ButtonGroup>
-      </Pagination.Root>
-    </Stack>
+    </Table.ScrollArea>
   );
 };
 
